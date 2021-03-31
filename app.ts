f0c8b10a69017da17c921e8b0120d0c79afa127c
Md. Mahmudul Hasan Faisal <bdmhfaisal@gmail.com>
2021-03-31 16:28:02 +0600

    Core Types :: number, string & boolean

--- app.ts
+++ app.ts
@@ -1,13 +1,18 @@
-function Add(num1: number, num2: number) {
+function Add(num1: number, num2: number, checked: boolean, phrase: string) {
     // if(typeof num1 !== "number" || typeof num2 !== "number"){
     //     throw new Error("Unknown Type!");
     // }
-    return num1 + num2;
+    let result = num1 + num2;
+    if(checked){
+        console.log(phrase + result);
+    }
+    else{
+        return result;
+    }
 }
 
 const a = /*"12"*/ 12;
 const b = 14.141;
+const checkedType = !(typeof a !== "number" || typeof b !== "number");
 
-const result = Add(a,b);
-
-console.log(result);
+Add(a, b, checkedType, "Result is: ");
