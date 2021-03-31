9d96f1a944d34c8f47f4c1eb45202e9d60b57ba2
Md. Mahmudul Hasan Faisal <bdmhfaisal@gmail.com>
2021-03-31 16:09:10 +0600

    Ts type vs Js Type

--- app.ts
+++ app.ts
@@ -1,1 +1,13 @@
-console.log("form scratch");
+function Add(num1: number, num2: number) {
+    // if(typeof num1 !== "number" || typeof num2 !== "number"){
+    //     throw new Error("Unknown Type!");
+    // }
+    return num1 + num2;
+}
+
+const a = /*"12"*/ 12;
+const b = 14.141;
+
+const result = Add(a,b);
+
+console.log(result);
