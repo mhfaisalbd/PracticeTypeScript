66f87cd4ae5cbfb1e9aaeba6385e1e50c633ffcc
Md. Mahmudul Hasan Faisal <bdmhfaisal@gmail.com>
2021-04-01 10:32:05 +0600

    Enums & Custom Valued Enums

--- app.ts
+++ app.ts
@@ -1,32 +1,29 @@
-const student: {
-    name: string;
-    regNo: number;
-    id: number;
-    isCurrent: boolean;
-    courses: string[];
-    claims: [number, string];
-}
- = {
+// const student: {
+//     name: string;
+//     regNo: number;
+//     id: number;
+//     isCurrent: boolean;
+//     courses: string[];
+//     claims: [number, string];
+// }
+enum Role {Admin, Author, Moderator, Mentor};
+enum RoleWithCustom {Admin = "A", Author = 0, Moderator = "Application", Mentor=50};
+ const student = {
     name : "Hasan",
     regNo: 6530,
     id: 1602038,
     isCurrent: true,
     courses: ["CIT 111","CIT 112","PHY 111"],
-    claims: [2, "Author"]
+    claims: Role.Admin
 }
 
-// student.claims[1] = 3; //Error Dtected!
-// student.claims = [12,] //Error Detected!
-// student.claims = [12,"Admin", "Author"] //Error Detected!
-
-student.claims.push("Moderator"); //It is supported!
 
 // let myCourses: any[];  //It is possible, but breaks the features of TypeScript!!!
 // myCourses = [ true, "CIT 111", student, "CIT 112","PHY 111", 1];
 let myCourses: string[];
 myCourses = ["CIT 111","CIT 112","PHY 111"];
 
-console.log(student.name + ":");
+console.log(student.name + ":" + student.claims);
 for (const course of student.courses) {
     //console.log(course.map()); //Error Detected!
     console.log(course.toUpperCase());
