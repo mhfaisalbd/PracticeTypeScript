a529f9ec92f4b0d8f0adeaa98a2323d67daa5784
Md. Mahmudul Hasan Faisal <bdmhfaisal@gmail.com>
2021-04-01 09:46:58 +0600

    Tuple or Fixed Length Array

--- app.ts
+++ app.ts
@@ -1,17 +1,25 @@
-// const student: {
-//     name: string;
-//     regNo: number;
-//     id: number;
-//     isCurrent: boolean;
-// }
-
-const student = {
+const student: {
+    name: string;
+    regNo: number;
+    id: number;
+    isCurrent: boolean;
+    courses: string[];
+    claims: [number, string];
+}
+ = {
     name : "Hasan",
     regNo: 6530,
     id: 1602038,
     isCurrent: true,
-    courses: ["CIT 111","CIT 112","PHY 111"]
+    courses: ["CIT 111","CIT 112","PHY 111"],
+    claims: [2, "Author"]
 }
+
+// student.claims[1] = 3; //Error Dtected!
+// student.claims = [12,] //Error Detected!
+// student.claims = [12,"Admin", "Author"] //Error Detected!
+
+student.claims.push("Moderator"); //It is supported!
 
 // let myCourses: any[];  //It is possible, but breaks the features of TypeScript!!!
 // myCourses = [ true, "CIT 111", student, "CIT 112","PHY 111", 1];
