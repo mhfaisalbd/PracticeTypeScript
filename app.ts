ed96c8b5d9ca215602175b77f4e125e227fd46f0
Md. Mahmudul Hasan Faisal <bdmhfaisal@gmail.com>
2021-04-01 17:01:53 +0600

    literal types

--- app.ts
+++ app.ts
@@ -1,8 +1,15 @@
-function combine(input1: number | string, input2: number | string) {
-    if(typeof input1 === 'number'&& typeof input2 === 'number')
-        return input1 + input2;
+function combine(
+    input1: number | string, 
+    input2: number | string,
+    resultType: "text" | "number"
+    ) {
+    if(typeof input1 === 'number'&& typeof input2 === 'number' || resultType === "number")
+        return +input1 + +input2;
     return input1.toString().concat( ' ', input2.toString());
 }
 
-console.log(combine(100, 130));
-console.log(combine("Ariana", "Marie"));
+console.log(combine(100, 130, "number"));
+console.log(combine("Ariana", "Marie", "text"));
+
+console.log(combine(100, 130, "text"));
+console.log(combine("Leah", "Gotti", "number"));
