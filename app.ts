93847b37e0fe3b737daaac522a0fc3b76309b143
Md. Mahmudul Hasan Faisal <bdmhfaisal@gmail.com>
2021-04-01 11:25:07 +0600

    Union Types

--- app.ts
+++ app.ts
@@ -1,31 +1,8 @@
-// const student: {
-//     name: string;
-//     regNo: number;
-//     id: number;
-//     isCurrent: boolean;
-//     courses: string[];
-//     claims: [number, string];
-// }
-enum Role {Admin, Author, Moderator, Mentor};
-enum RoleWithCustom {Admin = "A", Author = 0, Moderator = "Application", Mentor=50};
- const student = {
-    name : "Hasan",
-    regNo: 6530,
-    id: 1602038,
-    isCurrent: true,
-    courses: ["CIT 111","CIT 112","PHY 111"],
-    claims: Role.Admin
+function combine(input1: number | string, input2: number | string) {
+    if(typeof input1 === 'number'&& typeof input2 === 'number')
+        return input1 + input2;
+    return input1.toString().concat( ' ', input2.toString());
 }
 
-
-// let myCourses: any[];  //It is possible, but breaks the features of TypeScript!!!
-// myCourses = [ true, "CIT 111", student, "CIT 112","PHY 111", 1];
-let myCourses: string[];
-myCourses = ["CIT 111","CIT 112","PHY 111"];
-
-console.log(student.name + ":" + student.claims);
-for (const course of student.courses) {
-    //console.log(course.map()); //Error Detected!
-    console.log(course.toUpperCase());
-
-}
+console.log(combine(100, 130));
+console.log(combine("Ariana", "Marie"));
