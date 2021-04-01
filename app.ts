6f22175f32633929238537b8d53d1eec2ebbb611
Md. Mahmudul Hasan Faisal <bdmhfaisal@gmail.com>
2021-04-01 09:25:17 +0600

    Arrays

--- app.ts
+++ app.ts
@@ -9,7 +9,18 @@ const student = {
     name : "Hasan",
     regNo: 6530,
     id: 1602038,
-    isCurrent: true
+    isCurrent: true,
+    courses: ["CIT 111","CIT 112","PHY 111"]
 }
 
-console.log(student.name);
+// let myCourses: any[];  //It is possible, but breaks the features of TypeScript!!!
+// myCourses = [ true, "CIT 111", student, "CIT 112","PHY 111", 1];
+let myCourses: string[];
+myCourses = ["CIT 111","CIT 112","PHY 111"];
+
+console.log(student.name + ":");
+for (const course of student.courses) {
+    //console.log(course.map()); //Error Detected!
+    console.log(course.toUpperCase());
+
+}
