6c1fe65c213600b44cbfb55f7dba0f95f87d825b
Md. Mahmudul Hasan Faisal <bdmhfaisal@gmail.com>
2021-04-04 10:57:54 +0600

    Function types

--- app.ts
+++ app.ts
@@ -1,17 +1,14 @@
-function Add(inputA:number, inputB: number) /*: number*/ {
+function Add(inputA:number, inputB: number) {
     return inputA + inputB;
 }
 
-
-//Valided void type where haven't requrie of return statement
 function PrintResult(input:number): void {
     console.log("The result is: "+input);
 }
 
-//undefined requires at least any of return statement
-function PrintResultAndReturn(input:number): undefined {
-    console.log("The result is: "+input);
-    return;
-}
+let combine: (a:number, b:number) => number;
+combine = Add;
+// combine = PrintResult;
+// combine = 5;
 
-console.log(PrintResult(Add(12, 14)));
+console.log(PrintResult(combine(12, 14)));
