919e167c21aa6c80d90621238fdd9bc496df6b3e
Md. Mahmudul Hasan Faisal <bdmhfaisal@gmail.com>
2021-04-05 00:54:38 +0600

    Advantage of arrow functions

--- src/app.ts
+++ src/app.ts
@@ -9,10 +9,12 @@ console.log(myVar); // Accessable in outer/global scope.
 // console.log(myConst); // Not Accessable in outer scope!
 // console.log(myLet); // // Not Accessable in outer scope!
 
+const add = (a:number, b:number) => a + b;
 
+const printOut : (data: number | string) => void = input => console.log(input);
 
 let btn = document.querySelector('button');
 
-btn?.addEventListener('click', ()=>{
-    console.log('clicked!')
-});
+btn?.addEventListener('click', event => console.log(event));
+
+printOut(add(12,24));
