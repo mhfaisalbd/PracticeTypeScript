332837a9d5730213d32b91daa7ba54a02a05781d
Md. Mahmudul Hasan Faisal <bdmhfaisal@gmail.com>
2021-04-05 00:44:38 +0600

    diff let const and var

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
-// function SendAnalytics(data: string) {
-//     console.log(data);
-// }
+{
+    var myVar = "1952";
+    const myConst = "2312";
+    // myConst = "1234"; //Error Detected!
+    let myLet = "3211";
+    myLet = "1122";
+}
+console.log(myVar); // Accessable in outer/global scope.
+// console.log(myConst); // Not Accessable in outer scope!
+// console.log(myLet); // // Not Accessable in outer scope!
 
-// SendAnalytics("Brain Washed!");
 
-let btn = document.querySelector('button')!;
 
-btn.addEventListener('click', ()=>{
+let btn = document.querySelector('button');
+
+btn?.addEventListener('click', ()=>{
     console.log('clicked!')
 });
