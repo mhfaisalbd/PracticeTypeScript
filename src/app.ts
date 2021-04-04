90342615b44bd0be1d89f1503ad9779782090b2c
Md. Mahmudul Hasan Faisal <bdmhfaisal@gmail.com>
2021-04-04 17:05:25 +0600

    Configuring TypeScript Compilation process

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+// function SendAnalytics(data: string) {
+//     console.log(data);
+// }
+
+// SendAnalytics("Brain Washed!");
+
+let btn = document.querySelector('button')!;
+
+btn.addEventListener('click', ()=>{
+    console.log('clicked!')
+});
