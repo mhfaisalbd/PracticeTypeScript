f007ddafdc6a4a0f0ab7d053be76cf69d11d64f5
Md. Mahmudul Hasan Faisal <bdmhfaisal@gmail.com>
2021-04-06 13:18:01 +0600

    Usage of 'this' keyword in ctor function

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,14 @@ class OperatingSystem {
     constructor(n: string){
         this.name = n;
     }
+
+    printOS(this: OperatingSystem){
+        console.log(this.name);
+    }
 }
 
 const windows10 = new OperatingSystem("Windows 10");
-console.log(windows10);
+windows10.printOS();
+
+const ubuntu = { name: "Ubuntu 20.4", printOS : windows10.printOS };
+ubuntu.printOS();
