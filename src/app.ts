405eb10b79bea8ca7413328f3da42ef2727ac8c3
Md. Mahmudul Hasan Faisal <bdmhfaisal@gmail.com>
2021-04-10 14:22:01 +0600

    Optinal Properties and parameters in Interface

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
 interface MultiPly{
-    (numA: number, numB: number) : number;
+    (numA: number, numB?: number) : number;
 }
 
 let mul: MultiPly;
-mul = (n1: number, n2: number) => {
+mul = (n1: number, n2: number = 8) => {
     return n1 * n2;
 }
 
-console.log(mul(8, 9));
+console.log(mul(8));
 
 interface TergetingSite{
     targetSite: string;
@@ -21,14 +21,14 @@ interface Hack extends TergetingSite, HackStatus {
     hack(callBack: ()=> void) : void;
 }
 interface HackerInfo{
-    hackerName : string;
-    hackerIP : string;
+    hackerName? : string;
+    hackerIP? : string;
 }
 class FacebookHack implements Hack, HackerInfo{
     targetSite: string;
     
-    hackerName: string;
-    hackerIP: string;
+    hackerName?: string;
+    hackerIP?: string;
 
     status: "Hacked" | "In Progress" | "Scheduled";
     hack(callBack: () => void): void {
@@ -38,7 +38,7 @@ class FacebookHack implements Hack, HackerInfo{
         callBack();
     }
 
-    constructor(n: string, ip: string){
+    constructor(n?: string, ip?: string){
         this.targetSite = "https://facebook.com";
         this.status = "Scheduled";
         this.hackerName = n;
