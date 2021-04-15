a12f64f8cd7a8fefb13af6e2e3adb1cf77d65fa3
Md. Mahmudul Hasan Faisal <bdmhfaisal@gmail.com>
2021-04-15 18:54:56 +0600

    Type Guards

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ type User = {
     startingDate: Date;
 }
 
-type AppUser = User & Admin;
+type AppUser = User | Admin;
 
 const user1: AppUser = {
     name: "Abu Saleh",
@@ -16,7 +16,51 @@ const user1: AppUser = {
     startingDate: new Date()
 }
 
-type Custom1 = string | number;
-type Custom2 = number | boolean;
+function printInfo(user:AppUser) {
+    console.log('Name: '+ user.name);
+    if('role' in user)
+        console.log('Role: '+ user.role);
+    if('startingDate' in user)
+        console.log('Name: '+ user.startingDate);
 
-type OwnCustom = Custom1 & Custom2;
+}
+
+printInfo(user1);
+printInfo({name: "Ahamd Alp", startingDate: new Date()});
+
+// type Custom1 = string | number;
+// type Custom2 = number | boolean;
+
+// type OwnCustom = Custom1 & Custom2;
+
+
+class ButtonPhone {
+    phoneCall(dailNumber: number){
+        console.log('Dailing...\n to'+ dailNumber);
+    }
+}
+
+class SmartPhone {
+    phoneCall(dailNumber: number){
+        console.log('Dailing...\n to'+ dailNumber);
+    }
+    videoCall(userName: string){
+        console.log('Dailing...\n to'+ userName);
+    }
+    
+}
+
+type Phone = ButtonPhone | SmartPhone;
+
+const myPhone1 = new ButtonPhone();
+const myPhone2 = new SmartPhone();
+
+function usePhone(phone:Phone) {
+    phone.phoneCall(1234);
+    if(phone instanceof SmartPhone)   {
+        phone.videoCall('ALTAP');
+    } 
+}
+
+usePhone(myPhone1);
+usePhone(myPhone2);
